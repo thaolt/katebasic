690827e75e10777f2aa868d30ffb7db3b24340ef
thaolt <thaolt@songphi.com>
2019-06-11 05:08:16 +0700

    update test assembly

--- kas/test.asm
+++ kas/test.asm
@@ -1,34 +1,69 @@
 include 'c8asm.inc'
+; Companion Cube Sample program. Disregard its advice.
 
-; test
+; Assemble the program to a CHIP-8 binary like so:
+;    $ ./c8asm -o GAMES/CUBE8.ch8 examples/cube.asm
+; Once you've assembled it, you can run it in the interpreter like so:
+;    $ ./chip8 -f FFB6F8 -b B2B2B2 GAMES/CUBE.ch8
 
-start: 
-cls
-jmp 0x234
-bjp 0x567
-shr v1, v2
-mov va, 0x88
-mov va, vb
-mov va, dtm
-mov i, 0xabc
-add vb, 2
-add i, v9
-drw v1, v2, 8
-xdrw v5, v9
-rnd va, 100
+; Define some more human-friendly names for the registers
+; define v0 V0 ; Sprite X,Y position
+;define v1 V1
+;define v2 V2 ; Previous sprite X,Y position
+;define v3 V3
+;define v4 V4 ; Sprite direction
+;define v5 V5
+;define ve VE
 
-se va, vb
-se vc, 0x66
+; Clear the screen
+CLS
+	
+; Load the variables' initial values
+mov v0, 1
+mov v4, 1
+mov v1, 10
+mov I, sprite1
+DRW v0, v1, 8
+mov ve, 1  
 
-sne v8, v9
-sne vd, 0x99
+; The main loop
+loop:
+	; Store the current position 
+mov v2, v0
+mov v3, v1
+	
+	; If the X direction is 0, go to sub1...
+SE v4, 0
+JMP sub1
+	
+	; ...otherwise add 1 to the box' position 
+ADD v0, 1
 
-regd vf
-call print_sth
+	; If you reached the right edge of the screen, change direction
+	SNE v0, 56
+mov v4, 1
+jmp draw1
+sub1: 
+	; subtract 1 from the box' position
+SUB v0, ve
+	
+	; If you reached the left edge of the screen, change direction
+SNE v0, 0
+mov v4, 0 
 
-halt
+; Draw the box
+draw1:  
+	; Load the address of the sprite's graphics into register I
+mov I, sprite1
+	; Erase the sprite at the old position
+DRW v2, v3, 8
+	; Draw the sprite at the new position
+DRW v0, v1, 8
 
-print_sth:
-xdrw vd, ve
-ret
+	; Return to the start of the loop
+	JMP  loop
 
+; Binary data of the sprite.
+; 1s represent pixels to be drawn, 0s are blank pixels.
+sprite1:
+db  01111110b, 10000001b, 10100101b, 10111101b, 10111101b, 10011001b, 10000001b, 01111110b
